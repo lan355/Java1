a633ff5029509bfbbbcafeae68c65af6be4da664
lan355 <48014015+lan355@users.noreply.github.com>
2020-09-16 17:29:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,39 @@
 import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        Airport airport = new Airport();
+
         Airports airports = new Airports();
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите имя: ");
-        airport.setFirstname(scanner.nextLine());
-        System.out.print("Введите фамилию: ");
-        airport.setMiddlename(scanner.nextLine());
-        System.out.print("Введите Отчество:");
-        airport.setLastname(scanner.nextLine());
-        System.out.print("Введите номер рейса:");
-        airport.setNumberFlight(scanner.nextLine());
-        System.out.print("Введите номер багажной квитанции:");
-        airport.setNumberBag(scanner.nextLine());
-        System.out.print("Выберите класс багажа: \n 1)Эконом Класс = 1место не более 23 кг \n 2)Бизнес класс 2 места по 23 кг \n");
-        airport.setNumberOfSeatsBag(scanner.nextInt());
-        System.out.print("Введите суммарный вес багажа:");
-        airport.setSummWeightOfBag(scanner.nextInt());
 
-        System.out.print("\n");
-        System.out.print(airport.toString());
-
-        System.out.print("\n");
+        airports.addPassenger(new Airport("Bill","Velington","Mihael","r3ss34","r432",2,46));
+        airports.addPassenger(new Airport("Andrew","Coronov","Alexeyevich","et2ts34","2tyt3",1,25));
+        airports.addPassenger(new Airport("Alex","Maicon","Keylor","gr45t","ef434",2,50));
+        System.out.println("Выберите что хотите дальше сделать:" + "\n" + "1) Добавить пассажира\n" + "2)Вывести информацию о пассажирах,вес багажа которых превышает 30 кг\n"
+                + "3)Вывести информацию о суммарном весе багажа всех пассажиров рейса\n" + "4)Вывести информацию по заданному номеру багажной квитанции\n" + "5)Удалить запись о пассажире с заданной фамилией\n" + "6)Выход" + "\n" + "Ваш выбор:");
+        Scanner scanner =new Scanner(System.in);
         while (true) {
-            System.out.println("Выберите что хотите дальше сделать:" + "\n" + "1) Добавить пассажира\n" + "2)Вывести информацию о пассажирах,вес багажа которых превышает 30 кг\n"
-                    + "3)Вывести информацию о суммарном весе багажа всех пассажиров рейса\n" + "4)Вывести информацию по заданному номеру багажной квитанции\n" + "5)Удалить запись о пассажире с заданной фамилией\n" + "6)Выход");
 
             switch ((int) check(scanner))
             {
                 case 1:
-
+                    addPassenger(airports,scanner);
+                    break;
+                case 2:
+                    System.out.print(airports.ShowMassOver30());
+                    break;
+                case 3:
+                    System.out.print(airports.summWeightOfBag());
+                    break;
+                case 4:
+                    System.out.print(airports.numberOfBag(scanner.nextLine()));
+                    break;
+                case 5:
+                    System.out.print(airports.removeLastName(scanner.nextLine()));
+                    break;
+                case 6:
+                    return;
             }
         }
     }
@@ -43,13 +45,34 @@ public class Main {
             String s = in.nextLine();
             try
             {
-                float a = Float.parseFloat(s);
-                return a;
+                return Float.parseFloat(s);
             } catch (NumberFormatException e)
             {
-                System.out.println("Неверный ввод данных, пожалуйста попробуйте нова");
+
             }
         }
+    }
+
+    static void addPassenger(Airports airport, Scanner scanner)
+    {
+        System.out.print("Введите имя: ");
+        String firstname = scanner.nextLine();
+        System.out.print("Введите фамилию: ");
+        String middlename = scanner.nextLine();
+        System.out.print("Введите Отчество:");
+        String lastname = scanner.nextLine();
+        System.out.print("Введите номер рейса:");
+        String numberFlight = scanner.nextLine();
+        System.out.print("Введите номер багажной квитанции:");
+        String numberBag = scanner.nextLine();
+        System.out.print("Выберите класс багажа: \n 1)Эконом Класс = 1место не более 23 кг \n 2)Бизнес класс 2 места по 23 кг \n");
+        int numberOfSeatsBag = scanner.nextInt();
+        System.out.print("Введите суммарный вес багажа:");
+        int summWeightOfBag = scanner.nextInt();
+
+        airport.addPassenger(new Airport(firstname,middlename,lastname,numberFlight,numberBag,numberOfSeatsBag,summWeightOfBag));
+        System.out.println("Выберите что хотите дальше сделать:" + "\n" + "1) Добавить пассажира\n" + "2)Вывести информацию о пассажирах,вес багажа которых превышает 30 кг\n"
+                + "3)Вывести информацию о суммарном весе багажа всех пассажиров рейса\n" + "4)Вывести информацию по заданному номеру багажной квитанции\n" + "5)Удалить запись о пассажире с заданной фамилией\n" + "6)Выход" + "\n" + "Ваш выбор:");
     }
 
 }
